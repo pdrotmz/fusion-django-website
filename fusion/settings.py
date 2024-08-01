5f904718aa8677b6ce107af8cfe839f98b66a9b7
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-01 17:00:35 -0300

    feat(settings): add email backend

--- fusion/settings.py
+++ fusion/settings.py
@@ -132,3 +132,20 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email de teste console
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+"""
+
+# Email produção
+
+EMAIL_HOST = 'localhost'
+EMAIL_HOST_USER = 'no-reply@teiastudios.com.br'
+EMAIL_PORT = 587
+EMAIL_USE_TSL = True
+EMAIL_HOST_PASSWORD = 'disruprtivo123'
+DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
+
+"""
