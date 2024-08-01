a5447360a446de8c4bf51c6fd6b954596c414925
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-01 00:32:51 -0300

    feat(views): add views of models on templates

--- app/views.py
+++ app/views.py
@@ -1,5 +1,14 @@
+from typing import Any
 from django.views.generic import TemplateView
+from .models import Servico, Disruptivo
 
 class IndexView(TemplateView):
     template_name = 'index.html'
-    
+
+    def get_context_data(self, **kwargs):
+        context =  super(IndexView, self).get_context_data(**kwargs)
+        context['servicos'] = Servico.objects.order_by('?').all()
+        context['disruptivos'] = Disruptivo.objects.order_by('?').all()
+        return context
+        
+
