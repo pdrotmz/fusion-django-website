ed3103c6154a82ae0f0c4ea7e5e908262dc41bd5
pdrotmz <pedrotomaz0210@gmail.com>
2024-08-01 00:34:03 -0300

    feat(admin): add admin register on templates

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Cargo, Servico, Disruptivo
 
-# Register your models here.
+@admin.register(Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    list_display = ('cargo', 'ativo', 'modificado')
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ('servico', 'icone', 'ativo', 'modificado')
+
+@admin.register(Disruptivo)
+class DisruptivoAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cargo', 'ativo', 'modificado')
+
+    
